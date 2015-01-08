78154266a55efc3e11af1ddd500d65bbf4b49a68
Marvin T <contact@marvinkturner.com>
2015-01-08 04:06:44 -0500

    add new say_goodnight program

---
+++ ch2/gn.rb
@@ -0,0 +1,10 @@
+#tell someone good night
+
+def say_goodnight (name)
+	result = "Good night, " + name
+	return result
+end
+
+#Let's call it
+puts say_goodnight("Marvin")
+puts say_goodnight("Everyone")
